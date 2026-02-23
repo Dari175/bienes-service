230a9c0e3f3004216ae2954ef28801a2cb48c575
Arturo Darinel Lopez Castillo <arturdar16@gmail.com>
2026-02-23 16:43:14 -0600

    ajuste de conexion de bd PatrimonioDB

--- src/config/db.js
+++ src/config/db.js
@@ -6,9 +6,14 @@ dns.setDefaultResultOrder("ipv4first");
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "patrimonioDB"  // ← agrega esto con el nombre exacto de tu BD en Atlas
+      dbName: "PatrimonioDB"  // ← agrega esto con el nombre exacto de tu BD en Atlas
     });
     console.log(`✅ MongoDB conectado: ${conn.connection.host}`);
+    console.log("DB NAME:", conn.connection.name);
+console.log("DB HOST:", conn.connection.host);
+
+const collections = await mongoose.connection.db.listCollections().toArray();
+console.log("COLLECTIONS:", collections.map(c => c.name));
   } catch (error) {
     console.error(`❌ Error al conectar MongoDB: ${error.message}`);
     process.exit(1);
